32a6ff5a0878fa2c8e5054e8bd13e3b6bec26ccc
fandrzejczak <106021371+fandrzejczak@users.noreply.github.com>
2023-03-01 05:06:51 +0100

    Dodaj pliki projektów.

---
+++ monte/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace monte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int g = 100000;
+            int v = 0;
+
+            double x, y, pi=0;
+            Random r = new Random();
+            for (int i = 1; i <= g; i++)
+            {
+                x = r.NextDouble();
+                y = r.NextDouble();
+                if ((x * x + y * y) <= 1)
+                    v++;
+                pi = 4 * (float)v / (float)i;
+            }
+            try
+            {
+                StreamWriter sw = new StreamWriter("C:\\monte\\monte.txt");
+                sw.WriteLine(pi);
+                sw.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            Console.WriteLine(pi);
+            Console.ReadLine();
+        }
+    }
+}
